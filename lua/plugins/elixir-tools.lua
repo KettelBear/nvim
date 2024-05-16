e9401396a1dc1346c6c877b0488a8123d01a5f9f
KettelBear <kettel.bear@gmail.com>
2024-05-15 21:38:24 -0400

    Update Elixir Tools Options

--- lua/plugins/elixir-tools.lua
+++ lua/plugins/elixir-tools.lua
@@ -4,13 +4,12 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   config = function()
     local elixir = require("elixir")
-    -- local elixirls = require("elixir.elixirls")
 
     elixir.setup {
       nextls = {
         enable = true,
-        -- cmd = "",
-        on_attach = function(client, bufnr)
+        cmd = "/home/kettelbear/.local/share/nvim/mason/packages/nextls/next_ls_linux_amd64",
+        on_attach = function ()
           local opts = { buffer = true, noremap = true }
 
           vim.keymap.set("n", "<leader>fp", ":ElixirFromPipe<cr>", opts)
@@ -22,22 +21,11 @@ return {
           vim.keymap.set("n", "<leader>co", ":Mix compile<CR>")
         end,
         init_options = {
-          experimental = { completions = { enable = false } }
+          experimental = { completions = { enable = true } }
         }
       },
       credo = { enable = true },
       elixirls = { enable = false }
-      --[[
-        -- The rest of the elixirls config
-        cmd = "",
-        settings = elixirls.settings({
-          fetchDeps = true,
-          dialyzerEnabled = true,
-          enableTestLenses = false,
-        }),
-        on_attach = on_attach,
-      },
-      ]]--
     }
   end,
   dependencies = {
